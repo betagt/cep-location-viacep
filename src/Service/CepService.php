efeeef9697dcc4ef8564e6a7ec4ddbb71f0f5404
diuliano de sousa oliveira <diuliano@betagt.com.br>
2017-01-31 13:34:03 -0300

    iniciando biblioteca

---
+++ src/Service/CepService.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dsoft
+ * Date: 05/08/2016
+ * Time: 17:45
+ */
+
+namespace ProcuraLivre\Services;
+
+
+use Dmitrovskiy\IonicPush\Exception\RequestException;
+use ProcuraLivre\Repositories\CidadeRepository;
+use ProcuraLivre\Repositories\EstadoRepository;
+
+class CepService
+{
+
+    private static $endpoint = 'http://viacep.com.br/ws/';
+
+    const RETURN_TYPE_JSON = 'json';
+
+    const RETURN_TYPE_XML = 'xml';
+
+    const RETURN_TYPE_PIPED = 'piped';
+
+    const RETURN_TYPE_QUERTY = 'querty';
+
+    /**
+     * @var CidadeRepository
+     */
+    private $cidadeRepository;
+    /**
+     * @var EstadoRepository
+     */
+    private $estadoRepository;
+    /**
+     * @var CacheService
+     */
+    private $cacheService;
+
+    public function __construct(CidadeRepository $cidadeRepository, EstadoRepository $estadoRepository, CacheService $cacheService)
+    {
+
+        $this->cidadeRepository = $cidadeRepository;
+        $this->estadoRepository = $estadoRepository;
+        $this->cacheService = $cacheService;
+    }
+
+    /**
+     * @param int $cep
+     * @return array
+     * @throws RequestException
+     */
+    public function requestCep($cep,$type = self::RETURN_TYPE_JSON){
+        $json_file = $this->builderUrl($cep,$type);
+        if(array_key_exists('erro',$json_file)){
+            throw new RequestException("CEP Inválido!");
+        }
+        return $json_file;
+    }
+
+    /**
+     * @param array $args ['estado','cidade','endereco']
+     * @return mixed
+     * @throws RequestException
+     */
+    public function cepLocation($args,$type = self::RETURN_TYPE_JSON){
+        $result = $this->builderUrl($args,$type);
+
+        return $result;
+    }
+
+    private function builderUrl($args, $type){
+
+        if(is_array($args))
+            $args = implode('/',$args);
+
+        $args = self::$endpoint.$args.'/'.$type.'/';
+
+        if(!$this->cacheService->has('zipcode_'.$args)){
+
+            $result = $this->strategyConvert($args,$type);
+            if(array_key_exists('erro',$result)){
+                throw new RequestException("Localidade Inválida!");
+            }
+
+            $this->cacheService->put('zipcode_'.$args,$result);
+        }
+        return (array)$this->cacheService->get('zipcode_'.$args);
+    }
+
+    private function strategyConvert($args , $type){
+        switch ($type){
+            case self::RETURN_TYPE_JSON:
+                return json_decode(file_get_contents($args));
+                break;
+            case self::RETURN_TYPE_XML:
+                return file_get_contents($args);
+                break;
+        }
+    }
+}
